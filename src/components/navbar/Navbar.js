b7f54dac7c7a371d7fd9d2a295db2a679dd18f13
Jason Brunch <jasonbunce2@gmail.com>
2024-06-24 22:13:01 -0700

    CSS migration complete added some links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import MailSVG from "../../assets/mail.svg";
 import { useEffect, useState } from "react";
 import { Link } from "gatsby";
-import "./Navbar.css";
+import "./navbar.css";
 
 
 const Navbar = ({ data }) => {
@@ -44,6 +44,19 @@ const Navbar = ({ data }) => {
           <h3>JASON BUNCE</h3>
           <div className="circle"></div>
         </div>
+
+      <div className="nav-links">
+        <Link to="/" className="nav-link">
+          <h5>PORTFOLIO</h5>
+        </Link>
+        <Link to="/" className="nav-link">
+          <h5>COMPONENTS</h5>
+        </Link>
+        <Link to="/journal" className="nav-link">
+          <h5>JOURNAL</h5> 
+        </Link>
+      </div>
+
         <Link to="/contact-page" className="CTA">
           <MailSVG className="mail-svg" />
           <h5>GET IN TOUCH</h5>
---
+++ src/pages/journal.js
@@ -0,0 +1,107 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import Card from "../components/cards/Card";
+import beachImage from "../images/beach-concept.jpg";
+import JournalSVG from "../assets/journal.svg";
+import QuoteSVG from "../assets/quote.svg";
+import { useEffect, useRef } from "react";
+import "../styles/journal.css";
+
+
+
+
+
+const Journal = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges;
+  const blogPostContainerRef = useRef(null);
+
+
+
+
+  /*TEXT MOVEMENT CODE*/
+  useEffect(() => {
+    const observerOptions = {
+      root: null, // Use the viewport as the root
+      rootMargin: '0px 0px 20% 0px', // Start the animation 20% before the element enters the viewport
+      threshold: 0, // Trigger when any part of the element is visible
+    };
+
+    const observerCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        } else {
+          entry.target.classList.remove('visible');
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    if (blogPostContainerRef.current) {
+      observer.observe(blogPostContainerRef.current);
+    }
+
+    return () => {
+      if (blogPostContainerRef.current) {
+        observer.unobserve(blogPostContainerRef.current);
+      }
+    };
+  }, []);
+
+
+
+
+  return (
+    <Layout>
+     
+      <section className="image-container">
+        <div className="background-image" style={{ backgroundImage: `url(${beachImage})` }}>
+          <JournalSVG className="journal-svg " />
+        </div>
+      </section>
+      <section className="quote-container">
+        <QuoteSVG className="quote-svg" />
+        <h4>WELCOME TO THE JOURNAL</h4>
+      </section>
+      <section className="blog-post-container move-up-text"
+      ref={blogPostContainerRef}
+      >
+        {posts.map(({ node }) => (
+          <Card
+            key={node.id}
+            title={node.frontmatter.title}
+            description={node.frontmatter.description}
+            path={node.frontmatter.path}
+            image={node.frontmatter.image.childImageSharp.gatsbyImageData}
+          />
+        ))}
+      </section>
+   
+    </Layout>
+  );
+};
+export const query = graphql`
+  {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            description
+            path
+            image {
+              childImageSharp {
+                gatsbyImageData(width: 600, height: 400, layout: FULL_WIDTH)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default Journal;
