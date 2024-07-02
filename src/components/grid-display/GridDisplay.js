a76de7b93effd6e3d51ddb32e1cf41722ec9b4f3
Jason Brunch <jasonbunce2@gmail.com>
2024-07-01 23:43:33 -0700

    Added images to the grid component and experimented with its css

--- src/components/grid-display/GridDisplay.js
+++ src/components/grid-display/GridDisplay.js
@@ -1,18 +1,33 @@
 import "./grid-display.css";
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const GridDisplay = () => {
+  // GraphQL query to get the image
+  const data = useStaticQuery(graphql`
+    query {
+      componentLibraryImage: file(relativePath: { eq: "component-library.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 1920, quality: 100, layout: CONSTRAINED)
+        }
+      }
+    }
+  `);
+
+  // Get the image data using getImage helper function
+  const image = getImage(data.componentLibraryImage);
+
   return (
     <section className="grid-display-main-container">
-      <div className="grid-item tall">Project 1</div>
-      <div className="grid-item short">Project 2</div>
-      <div className="grid-item short">Project 3</div>
-      <div className="grid-item tall">Project 4</div>
-      <div className="grid-item short">Project 5</div>
-      <div className="grid-item short">Project 6</div>
-      <div className="grid-item tall">Project 7</div>
-      <div className="grid-item short">Project 8</div>
-      <div className="grid-item short">Project 9</div>
+      <div className="grid-item">
+        <GatsbyImage image={image} alt="Component Library" />
+      </div>
+      <div className="grid-item">Project 2</div>
+      <div className="grid-item">Project 3</div>
+      <div className="grid-item">Project 4</div>
+      <div className="grid-item">Project 5</div>
+      <div className="grid-item">Project 6</div>
     </section>
   );
 };
