92624b42f9ae8ba72ccb37631954cf9b86ce9785
Jason Brunch <jasonbunce2@gmail.com>
2024-08-21 22:14:44 -0700

    Finished implementing the placeholder for the technologies stack in the project pages

--- content/projects/project1.md
+++ content/projects/project1.md
@@ -2,19 +2,19 @@
 title: "Project 1"
 description: "A gym administration app to manage appointments, store workouts and prices, and more."
 path: "/projects/project1"
-image: "./project1-image.png"  # Make sure the image file is in the same directory
+image: "./project1-image.png"  # Ensure the image file is in the same directory
 date: "2024-08-20"
 technologies: ["React", "Gatsby", "GraphQL"]
+icons: 
+  - "./nextjs-icon.svg"
+  - "./nextjs-icon.svg"
+  - "./nextjs-icon.svg"
 ---
 
 #### DESCRIPTION
 
 Project 1 is a web application designed to help users manage their tasks more efficiently. It integrates with various third-party services to provide a seamless experience.
 
-
-
 #### TECHNOLOGIES
 
-- **React:** For building the user interface.
-- **Gatsby:** For static site generation.
-- **GraphQL:** To efficiently query data.
+This project uses React, Gatsby, and GraphQL.
--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -6,49 +6,60 @@ import "../styles/projects.css";
 import ButtonCoolShadow from "../components/buttons/button-cool-shadow";
 
 export default function ProjectTemplate({ data }) {
-    const project = data.markdownRemark;
-    const image = getImage(project.frontmatter.image);
+  const project = data.markdownRemark;
+  const image = getImage(project.frontmatter.image);
 
-    return (
-        <Layout>
-            <div className="project-container">
-                <div className="inner-container">
-                    {/* Project Image */}
-                    <GatsbyImage image={image} alt={project.frontmatter.title} />
+  return (
+    <Layout>
+      <div className="project-container">
+        <div className="inner-container">
+          {/* Project Image */}
+          <GatsbyImage image={image} alt={project.frontmatter.title} />
 
-                    {/* Project Title */}
-                    <h2>{project.frontmatter.title}</h2>
+          {/* Project Title */}
+          <h2>{project.frontmatter.title}</h2>
 
-                    {/* Description */}
-                    <p>{project.frontmatter.description}</p>
+          {/* Description */}
+          <p>{project.frontmatter.description}</p>
 
-                    {/* Buttons Section */}
-                    <div className="button-container">
-                        <ButtonCoolShadow text="View Project" link={project.frontmatter.path} />
-                        <ButtonCoolShadow text="Contact" link="/contact" />
-                    </div>
+          {/* Buttons Section */}
+          <div className="button-container">
+            <ButtonCoolShadow
+              text="View Project"
+              link={project.frontmatter.path}
+            />
+            <ButtonCoolShadow text="Contact" link="/contact" />
+          </div>
+        </div>
+        <div className="inner-container2">
+          {/* Project Content */}
+          <h6>IN DETAILS</h6>
+          <div className="project-content"
+            
+            dangerouslySetInnerHTML={{ __html: project.html }}
+          />
 
-                </div>
-                <div className="inner-container2">
-                    {/* Project Content */}
-                    <h6>IN DETAILS</h6>
-                    <div className="project-content" dangerouslySetInnerHTML={{ __html: project.html }} />
-
-                   
-                </div>
-
-
-
-
-
-
+          {/* Technologies Section */}
+          <div className="technologies-section">
+            <div className="icons-container">
+              {project.frontmatter.icons.map((icon, index) => (
+                <img
+                  key={index}
+                  src={icon.publicURL}
+                  alt={project.frontmatter.technologies[index]}
+                  className="tech-icon"
+                />
+              ))}
             </div>
-        </Layout>
-    );
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export const query = graphql`
-  query($id: String!) {
+  query ($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
@@ -60,7 +71,9 @@ export const query = graphql`
           }
         }
         technologies
-        path
+        icons {
+          publicURL
+        }
       }
     }
   }
