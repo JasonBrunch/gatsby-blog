0bd8280b90f93e5f83019ecdbf374cd1ed6e04ad
Jason Brunch <jasonbunce2@gmail.com>
2024-08-25 22:34:46 -0700

    Upgraded the mobile styling of the portfolio page

--- src/components/buttons/button-cool-shadow.js
+++ src/components/buttons/button-cool-shadow.js
@@ -1,9 +1,21 @@
 import React from "react";
 import './button-cool-shadow.css';
 
-const ButtonCoolShadow = ({title}) => {
+const colorOptions = {
+    red: "rgb(255 70 84/1)",
+    blue: "rgb(70 130 255/1)",
+    green: "rgb(70 255 84/1)",
+    white: "rgb(255, 255, 255/1)",  
+    transparent: "transparent", 
+};
+
+const ButtonCoolShadow = ({title, color}) => {
+
+    const styles = {
+        backgroundColor: colorOptions[color] || colorOptions.red
+      };
     return (
-        <button className="button-50">{title ? title : "Button 50"}</button>
+        <button className="button-50" style={styles}>{title ? title : "Button 50"}</button>
     );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,12 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
 import GridDisplay from "../components/grid-display/GridDisplay";
 import "../styles/portfolio.css";
-
 import Pointer from "../components/pointer/Pointer";
 import NextIcon from "../assets/nextjs-icon.svg";
 import NodeIcon from "../assets/node-icon.svg";
 import WordpressIcon from "../assets/wordpress-icon.svg";
 import ButtonCoolShadow from "../components/buttons/button-cool-shadow";
-
 import FullContactForm from "../components/contact/FullContactForm.js";
-
 
 const Home = ({ data }) => {
   // Get the image data using getImage helper function
@@ -21,105 +18,68 @@ const Home = ({ data }) => {
 
   return (
     <Layout>
-
-
-
       <div className="home-main-container">
-
         <section className="home-hero-main-container ">
           <div className="hero-secondary-containter ">
-
-
-
-            <div className="hero-image"><GatsbyImage image={portrait} alt="Me" className="portrait" /></div>
+            <div className="hero-image">
+              <GatsbyImage image={portrait} alt="Me" className="portrait" />
+            </div>
             <div className="hero-text ">
-              <h4 >Web Developer</h4>
-              <h2  >JASON BUNCE</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida lorem quis quam tristique euismod. </p>
-
+              <h4>Web Developer</h4>
+              <h2>JASON BUNCE</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                gravida lorem quis quam tristique euismod.{" "}
+              </p>
             </div>
-
             <div className="button-container">
-              <ButtonCoolShadow title="CONTACT" />
-              <ButtonCoolShadow title="SEE PROJECTS" />
+              <ButtonCoolShadow title="CONTACT" color="red" />
+              <ButtonCoolShadow title="SEE PROJECTS" color="transparent" />
             </div>
           </div>
-          <div className="pointer-container-container">
-            <Pointer title="About" className="pointer-container" />
-          </div>
-
-
-
+   
         </section>
-
-
         <section className="default-container">
           <div className="inner-container ">
             <div className="aboutme-wrapper ">
               <div className="aboutme-top-container ">
                 <div className="aboutme-top-container-title-wrapper">
-
                   <h1 className="">SERVICES & TECH</h1>
-
                   <div className="tech-stack-container-large ">
                     <NextIcon className="tech-stack-icon " />
                     <NodeIcon className="tech-stack-icon " />
                     <WordpressIcon className="tech-stack-icon " />
                     <WordpressIcon className="tech-stack-icon " />
-
-
                   </div>
-
-
                 </div>
-
-
-
-
-
                 <GatsbyImage image={coding} alt="Coding" className="portrait" />
-
               </div>
               <div className="aboutme-bottom-container">
-
                 <h5 className="">WEB DEVELOPER | WEB DESIGNER</h5>
-
-
                 <div className="tech-stack-container ">
                   <NextIcon className="tech-stack-icon " />
                   <NodeIcon className="tech-stack-icon " />
                   <WordpressIcon className="tech-stack-icon " />
                   <WordpressIcon className="tech-stack-icon " />
-
-
                 </div>
-                <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non posuere dolor.
-                  Vivamus et lectus at arcu rhoncus aliquam. Sed tincidunt, dui ut vulputate interdum,
-                  massa sapien sodales metus, a fermentum ex quam vitae nulla. Fusce non urna eleifend
-                  urna fermentum accumsan. Vivamus et lectus at arcu rhoncus.
+                <p className="">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  non posuere dolor. Vivamus et lectus at arcu rhoncus aliquam.
+                  Sed tincidunt, dui ut vulputate interdum, massa sapien sodales
+                  metus, a fermentum ex quam vitae nulla. Fusce non urna
+                  eleifend urna fermentum accumsan. Vivamus et lectus at arcu
+                  rhoncus.
                 </p>
 
                 <div className="button-container2">
                   <ButtonCoolShadow title="SEE JOURNAL" />
                   <ButtonCoolShadow title="PROJECTS" />
                 </div>
-
-
               </div>
-
-
-
             </div>
-
           </div>
-
-
         </section>
-        <Pointer title="PROJECTS" />
-
-
-
-
+       
 
         <GridDisplay />
         <FullContactForm />
@@ -135,12 +95,12 @@ export const query = graphql`
         gatsbyImageData(layout: CONSTRAINED)
       }
     }
-      image2: file(relativePath: { eq: "stockcoding.png" }) {
+    image2: file(relativePath: { eq: "stockcoding.png" }) {
       childImageSharp {
         gatsbyImageData(layout: CONSTRAINED)
+      }
+    }
   }
-}
-}
 `;
 
 export default Home;
